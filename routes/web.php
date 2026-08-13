c38785ec7a8a37e64b72fed1c3a7dd03edf7fd41
CSU22-AINF <CSU22@academico.unab.ad>
2026-08-13 12:07:47 -0500

    Php Basico

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,51 @@ Route::get('/', function () {
 });
 
 Route::get('/mi-path', function () {
-    return "Hola mundo";
+
+    //VARIABLES
+    $name = "Julian Godoy";
+    $age = 19;
+    $isLogin = true;
+    $height = 1.86;
+
+    echo "<h1> Hola como estan <p>Mi nombre es $name</p></h1>";
+
+    echo "<p> Tengo 19 años </p>";
+
+    //ARREGLOS
+
+    $names =[
+        "Santiago",
+        "Gabriela",
+        "Jaime",
+        "Santiago"
+    ];
+
+    $jesus = [
+        "name" => "Jesus",
+        "age" => 20,
+        "height" => 1.79,
+        "gener" => "male",
+        "Hobbies" => [
+            "MMA",
+            "Soccer",
+            "Motos"
+        ]
+    ];
+
+    foreach($names as $item){
+        echo "$item <br>";
+    }
+
+    echo validateAge(19);
+
+    return "================";
 });
+
+function validateAge($age){
+    if ($age >= 18){
+        return "Soy mayor de edad <br>";
+    }else{
+        return "Soy menor de edad <br>";
+    }    
+}
